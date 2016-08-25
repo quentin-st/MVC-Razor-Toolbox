d270119b45a44f6c7aa164ba6a00d54cddc86d04
Quentin Stoeckel <chteuchteu@gmail.com>
2016-08-25 16:50:47 +0200

    Add ReadOnlyForExtension

---
+++ content/Helpers/Razor/HtmlExtensions/ReadOnlyForExtension.cs.pp
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace $rootnamespace$.Helpers.Razor.HtmlExtensions
+{
+    public static class ReadOnlyForExtension
+    {
+        public static MvcHtmlString ReadOnlyFor<TModel, TValue>(this HtmlHelper<TModel> html,
+            Expression<Func<TModel, TValue>> expression)
+        {
+            return html.EditorFor(expression, new
+            {
+                @readonly = "readonly"
+            });
+        }
+    }
+}
