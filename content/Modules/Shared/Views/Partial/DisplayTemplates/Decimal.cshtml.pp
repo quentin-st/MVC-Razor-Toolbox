d0886e35f4b3f962aa14d431b33fb435ed1e8361
Quentin Stoeckel <chteuchteu@gmail.com>
2016-08-25 16:23:50 +0200

    Allow overriding decimal DisplayTemplate format

--- content/Modules/Shared/Views/Partial/DisplayTemplates/Decimal.cshtml.pp
+++ content/Modules/Shared/Views/Partial/DisplayTemplates/Decimal.cshtml.pp
@@ -1,6 +1,10 @@
+@using $rootnamespace$.Helpers.Razor
 @model object
 
 @{
+    var format = ViewBag.format != null ? (string) ViewBag.format : TemplatesConsts.DecimalFormat;
+
+    ViewBag.value = string.Format(format, Model);
     ViewBag.valueClass = "text-number";
 }
 
