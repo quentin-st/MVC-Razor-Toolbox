f7adba7986c842c7de909c711b580d47066e4aed
Quentin Stoeckel <chteuchteu@gmail.com>
2016-09-12 15:38:34 +0200

    Add autonumeric to decimal type

--- content/Areas/Shared/Views/Partial/EditorTemplates/Decimal.cshtml.pp
+++ content/Areas/Shared/Views/Partial/EditorTemplates/Decimal.cshtml.pp
@@ -1,8 +1,15 @@
-@using $rootnamespace$.Helpers.Razor
+@using System.Globalization
 @model decimal?
 
 @{
-    ViewBag.value = Model.HasValue ? string.Format(TemplatesConsts.DecimalFormat, Model.Value) : "";
+    ViewBag.htmlAttributes = new RouteValueDictionary
+    {
+        { "data-autonumeric", "true" }
+    };
+
+    // AutoNumeric needs dd.dd instead of dd,dd
+    var numberFormatInfo = new NumberFormatInfo { NumberDecimalSeparator = "." };
+    ViewBag.value = Model.HasValue ? String.Format(numberFormatInfo, "{0:0.00}", Model.Value) : "";
 }
 
 @Html.Partial("~/Areas/Shared/Views/Partial/EditorTemplates/TextBoxBase.cshtml")
